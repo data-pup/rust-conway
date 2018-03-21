16f09881934195ca13d6b258b8ed775e31c2c05b
data-pup <kevinmartinucr@gmail.com>
2018-03-21 10:05:57 -0700

    Removed tests for simple structs, renamed the position struct, and added a new file for validating the board state.

--- src/board/position.rs
+++ src/board/position.rs
@@ -1,23 +1,10 @@
-pub struct Position {
+pub struct BoardPosition {
     pub x: u32,
     pub y: u32,
 }
 
-impl Clone for Position {
-    fn clone(&self) -> Position {
-        return Position { x:self.x, y:self.y };
+impl Clone for BoardPosition {
+    fn clone(&self) -> BoardPosition {
+        return BoardPosition { x:self.x, y:self.y };
     }
-}
-
-#[cfg(test)]
-mod tests {
-    use board::position::*;
-
-    #[test]
-    fn test_positions() {
-        let p = Position { x:0, y:0 };
-        assert_eq!(p.x, 0);
-        assert_eq!(p.y, 0);
-    }
-
 }
