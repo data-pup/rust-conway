779301c21717d1065d382a2f65a76173ef0ed067
data-pup <kevinmartinucr@gmail.com>
2018-03-21 11:14:52 -0700

    Validate function now checks that the board is in a valid state.

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Rust Conway's Game of Life
 
-### Overview
+## Overview
 
 This is an implementation of Conway's Game of Life in Rust. This was a chance
 for me to practice learning to program in Rust some more, and learn some
@@ -10,3 +10,18 @@ Big picture concepts that I wanted to work on in this are:
 *  The module system
 *  Testing
 *  Documenting functions and structs
+
+## Rules
+
+To do ...
+
+## Difficulties Encountered & Lessons Learned
+
+### Testing
+
+To do ...
+
+### 'Sized' trait in static objects
+
+To do ...
+
--- src/board/validate.rs
+++ src/board/validate.rs
@@ -2,6 +2,10 @@ use board::Board;
 use board::position::BoardPosition;
 
 fn validate(b:&Board) -> bool {
+    let BoardPosition  { x:width,  y:height } = b.dims;
+    for &BoardPosition { x:curr_x, y:curr_y } in b.living {
+        if curr_x >= width || curr_y >= height { return false; }
+    }
     return true;
 }
 
@@ -18,17 +22,6 @@ mod tests {
         desc:            &'a str,
     }
 
-    static test_cases:[ValidateTestCase; 1] = [
-        ValidateTestCase {
-            dims:   BoardPosition { x:10, y:10 },
-            living: &[
-                BoardPosition { x:0, y:0 },
-            ],
-            expected_result: true,
-            desc: "10x10 Board with a single living square at origin.",
-        }
-    ];
-
     fn run_test(test_case:&ValidateTestCase) {
         let &ValidateTestCase {ref dims, living, expected_result, desc} = test_case;
         let b = Board { dims:dims.clone(), living:living.clone() };
@@ -40,4 +33,31 @@ mod tests {
     pub fn run_tests() {
         for case in test_cases.iter() { run_test(&case) }
     }
+
+    static test_cases:[ValidateTestCase; 4] = [
+        ValidateTestCase {
+            dims:   BoardPosition { x:10, y:10 },
+            living: &[BoardPosition { x:0, y:0 }],
+            expected_result: true,
+            desc: "10x10 Board with a single living square at origin.",
+        },
+        ValidateTestCase {
+            dims:   BoardPosition { x:5, y:5 },
+            living: &[BoardPosition { x:5, y:5 }],
+            expected_result: false,
+            desc: "5x5 Board with a single living square completely out of bounds.",
+        },
+        ValidateTestCase {
+            dims:   BoardPosition { x:5, y:5 },
+            living: &[BoardPosition { x:5, y:4 }],
+            expected_result: false,
+            desc: "5x5 Board with a single living square out of bounds on the x axis.",
+        },
+        ValidateTestCase {
+            dims:   BoardPosition { x:5, y:5 },
+            living: &[BoardPosition { x:4, y:5 }],
+            expected_result: false,
+            desc: "5x5 Board with a single living square out of bounds on the y axis.",
+        }
+    ];
 }
