f6ca369a4076668f2b5e77bee7c7cbba463f2bef
data-pup <kevinmartinucr@gmail.com>
2018-03-21 10:08:11 -0700

    Moved the board itself to the top of the module.

--- src/board/mod.rs
+++ src/board/mod.rs
@@ -1,5 +1,9 @@
-pub mod state;
 pub mod position;
+
+pub struct Board {
+    pub dims:   position::BoardPosition,
+    pub living: Vec<position::BoardPosition>,
+}
 
 pub fn hello() {
     println!("Hello, world!");
--- src/board/state.rs
+++
@@ -1,6 +0,0 @@
-use board::position::BoardPosition;
-
-pub struct BoardState {
-    pub dims:   BoardPosition,
-    pub living: Vec<BoardPosition>,
-}
