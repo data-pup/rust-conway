c35d0f7980752fd4bcc95a4e9377f2d159db8323
data-pup <kevinmartinucr@gmail.com>
2018-03-21 11:25:10 -0700

    Added doc string to the board position class.

--- src/board/position.rs
+++ src/board/position.rs
@@ -1,6 +1,15 @@
+/// Represents a position on the Conway's Game of Life Board.
+/// BoardPosition.x: The coordinate's position on the x axis.
+/// BoardPosition.y: The coordinate's position on the y axis.
 pub struct BoardPosition {
     pub x: u32,
     pub y: u32,
+}
+
+impl BoardPosition {
+    pub fn new(x:u32, y:u32) -> BoardPosition {
+        return BoardPosition { x:x, y:y };
+    }
 }
 
 impl Clone for BoardPosition {
