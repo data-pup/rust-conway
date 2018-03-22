e0f238d097e73a2f928488340a2591cb22965971
data-pup <kevinmartinucr@gmail.com>
2018-03-22 11:31:41 -0700

    Merged some of the board implementation logic into the mod.rs file. The ToString trait implementation will stay within a separate file, but these functions seemed trivial enough to place in the same file as the definition.

--- src/board/mod.rs
+++ src/board/mod.rs
@@ -1,12 +1,40 @@
 pub mod position;
 pub mod validate;
 pub mod board_to_string;
-pub mod board_new;
+// pub mod board_new;
 // pub mod update;
+
+use rand;
+use rand::Rng;
 
 use board::position::BoardPosition;
 
 pub struct Board {
     pub dims:   BoardPosition,
     pub living: Vec<BoardPosition>,
+}
+
+impl Board {
+    pub fn new(width: u32, height:u32) -> Board {
+        let dims = BoardPosition::new(width, height);
+        let living = Board::init_living(&dims);
+        return Board { dims:dims, living:living };
+    }
+
+    pub fn update(&mut self) {
+        let living = Board::init_living(&self.dims);
+        self.living = living;
+    }
+
+    fn init_living(dims:&BoardPosition) -> Vec<BoardPosition> {
+        let &BoardPosition {x:width, y:height} = dims;
+        let mut living = vec![];
+        let mut rng = rand::thread_rng();
+        for curr_x in 0..width {
+            for curr_y in 0..height {
+                if rng.gen() { &living.push(BoardPosition::new(curr_x, curr_y)); }
+            }
+        }
+        return living;
+    }
 }
