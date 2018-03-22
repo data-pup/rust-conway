e0297ee802765c1016accf00a1e14b3ac8a49dc3
data-pup <kevinmartinucr@gmail.com>
2018-03-22 13:02:10 -0700

    Cleaned up some code in the update file. The logic to find the number of neighbors for each position has now been moved into a helper function.

--- src/board/board_update.rs
+++ src/board/board_update.rs
@@ -10,27 +10,27 @@ impl Board {
             self.living = living;
         }
 
-        let neigh_counts: Vec<Vec<u32>> = self.living.iter()
-        .flat_map(|pos| Board::get_neighbors(pos, &self.dims))
-        .fold(Board::get_neighbor_count_matrix(&self.dims),
+        let neigh_counts = self.get_neighbor_counts();
+    }
+
+    /// Creates a matrix of unsigned integers representing the number of
+    /// living neighbors at each position on the board.
+    fn get_neighbor_counts(&self) -> Vec<Vec<u32>> {
+        self.living.iter()
+        .flat_map(|pos| Board::get_pos_neighbors(pos, &self.dims))
+        .fold(Board::init_neighbor_count_matrix(&self.dims),
             |mut counts, BoardPosition{x, y}| {
                 let (x_u, y_u) = (x as usize, y as usize);
                 counts[y_u][x_u] += 1;
                 counts
-            });
-
+            }
+        )
     }
-
-    /// This const contains relative steps to find each neighbor for a given position.
-    const NEIGHBOR_COORDS:[(i32, i32); 8] = [
-        (0, 1), (0, -1), (1, 0), (-1, 0),   // Up, down, right, left.
-        (1, 1), (1, -1), (-1, 1), (-1, -1), // Diagonal coordinates.
-    ];
 
     /// Map the position relative neighbor coordinate collections, filter out
     /// dimensions that are out of bounds, and collect the results into a
     /// vector of board position objects.
-    fn get_neighbors(pos:&BoardPosition, dims:&BoardPosition) -> Vec<BoardPosition> {
+    fn get_pos_neighbors(pos:&BoardPosition, dims:&BoardPosition) -> Vec<BoardPosition> {
         let (x, y) = (*&pos.x as i32, *&pos.y as i32);
         let (width, height) = (*&dims.x as i32, *&dims.y as i32);
         let neighbors = Board::NEIGHBOR_COORDS.iter()
@@ -43,7 +43,15 @@ impl Board {
         return neighbors;
     }
 
-    fn get_neighbor_count_matrix(dims:&BoardPosition) -> Vec<Vec<u32>> {
+    /// This const contains relative steps to find each neighbor for a given position.
+    const NEIGHBOR_COORDS:[(i32, i32); 8] = [
+        (0, 1), (0, -1), (1, 0), (-1, 0),   // Up, down, right, left.
+        (1, 1), (1, -1), (-1, 1), (-1, -1), // Diagonal coordinates.
+    ];
+
+    /// Allocates a matrix of u32 values that will be used to calculate the
+    /// number of neighbors that each position on the board has.
+    fn init_neighbor_count_matrix(dims:&BoardPosition) -> Vec<Vec<u32>> {
         let &BoardPosition {x:width, y:height} = dims;
         let (width_usize, height_usize) = (width as usize, height as usize);
         let new_living = vec![ vec![0; width_usize]; height_usize as usize ];
